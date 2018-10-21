eec8d69def24c678e924c7f122807dcde9ea83ce
Cesar Rodrigo Escobar Gutierrez <escobar.gutierrez.c.r@gmail.com>
2018-10-20 19:37:54 -0500

    screen3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,36 @@
 
-var SWAPI = function(){
+var page = function(){
 
 	function drawFilmsTable(filmsList) {
 			
-        var table = document.getElementById("filmsTable");
+        var table = document.getElementById("filmsTable")
 
         for (var i = 0; i < filmsList.length; i++) {
 
 			var filmsData = [filmsList[i].title,
 							 filmsList[i].director,
 							 filmsList[i].episode_id]
-			var filmNum = filmsList[i].url.charAt(filmsList[i].url.length - 2)
+			var filmNum = getUrlNumber(filmsList[i].url)
 
             var row = document.createElement("tr")
-            
+
             for (var k = 0; k < filmsData.length; k++) {
 
             	var cell = document.createElement("td")
 	            var cellText = document.createElement('span')
+	            cellText.setAttribute("class", "span-style")
 	            cellText.innerHTML = filmsData[k]
 	            cell.appendChild(cellText)
 	            row.appendChild(cell)
             }
 
-            var cell = document.createElement("td");
+            var cell = document.createElement("td")
             var cellBtn = document.createElement("a")
             cellBtn.setAttribute("class", "btn starship-btn lightsaber")
             cellBtn.setAttribute("role", "button")
             cellBtn.setAttribute("href", "screen2.html?" + filmNum)
      
-            cellBtn.innerHTML = "Starships"
+            cellBtn.innerHTML = "starships"
             cell.appendChild(cellBtn)
             row.appendChild(cell)
             
@@ -37,22 +38,87 @@ var SWAPI = function(){
         }
 	}
 
-	function fillCustomTable(data) {
+	function fillCustomTable(data, filmTitle) {
+
+		var shipNum = getUrlNumber(data.url)
 
         var table = document.getElementById("customTable")
 
         var row = document.createElement("tr")
-    	var cell = document.createElement("td");
-        var cellText = document.createElement('span')
-        cellText.innerHTML = data.name
+    	var cell = document.createElement("td")
+        var cellText = document.createElement('a')
+        cellText.setAttribute("role", "button")
+        cellText.innerHTML = data.name.toLowerCase()
+		cellText.setAttribute("class", "btn row-style")
+		cellText.setAttribute("href", "screen3.html?" + filmTitle + "&" + shipNum)
 
         cell.appendChild(cellText)
         row.appendChild(cell)
 		table.appendChild(row)
 	}
 
+	function fillDataTable(data){
+		
+		var keys = Object.keys(data)
+		var values = Object.values(data)
+
+		var table = document.getElementById("customTable")			
+
+		for (var i = 0; i < keys.length; i++) {
+				
+			if(!Array.isArray(values[i])){
+
+				var row = document.createElement("tr")
+				var cell = document.createElement("td")
+		        var cellText = document.createElement('span')        
+		        cellText.innerHTML = keys[i].replace(/_/g, " ") + " : " + values[i].toLowerCase()
+		        cellText.setAttribute("class", "span-style")
+
+		        cell.appendChild(cellText)
+		        row.appendChild(cell)
+				table.appendChild(row)
+			}
+		}			
+	}
+
+	function getUrlNumber(url) {
+		
+		var result = []
+		var k = url.length - 2
+		var number = ""
+
+		while(url.charAt(k) != "/"){
+			result.unshift(url.charAt(k))
+			k--
+		}
+
+		for (var i = 0; i < result.length; i++) {
+			number += result[i]
+		}
+		return number
+	}
+
+	function setText(type, name, text){
+
+		switch(type){
+
+			case "class":
+				var sections = document.getElementsByClassName(name)
+				for (var i = 0; i < sections.length; i++) {
+					sections[i].innerHTML = text.toLowerCase()
+				}
+			break
+
+			case "id":
+				document.getElementById(name).innerHTML = text.toLowerCase()
+			break
+		}
+	}
+
 	return{
 		drawFilmsTable:drawFilmsTable,
-		fillCustomTable:fillCustomTable
+		fillCustomTable:fillCustomTable,
+		setText:setText,
+		fillDataTable:fillDataTable
 	}
 }()
